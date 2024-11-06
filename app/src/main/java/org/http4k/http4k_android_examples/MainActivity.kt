987dc774c862d67d3c82c59359c19253cdb2fb13
Ivan Sanchez <ivan@gourame.com>
2024-11-06 08:48:40 +0000

    Initial commit

---
+++ app/src/main/java/org/http4k/http4k_android_examples/MainActivity.kt
@@ -0,0 +1,57 @@
+package org.http4k.http4k_android_examples
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.http4k.http4k_android_examples.ui.theme.Http4kandroidexamplesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Http4kandroidexamplesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /* TODO: Handle button click */ }) {
+            Text(text = "Make fuel call")
+        }
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+
+
+
